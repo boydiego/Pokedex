d0f620dbfaabc06ea32e24ff43b1615f182db1da
diego boy <diegolive5000@gmail.com>
2022-03-31 17:20:31 +0200

    wrap the pokemonList in an IIFE and update the forEach loop accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
-let pokemonList = [
-  {
-    name: 'Bulbasaur',
-    height: 0.7,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Charmander',
-    height: 0.6,
-    types: ['fire']
-  },
-  {
-    name: 'Squirtle',
-    height: 0.5,
-    types: ['water']
-  },
-  {
-    name: 'Ekans',
-    height: 2,
-    types:['poison']
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Charmander',
+      height: 0.6,
+      types: ['fire']
+    },
+    {
+      name: 'Squirtle',
+      height: 0.5,
+      types: ['water']
+    },
+    {
+      name: 'Ekans',
+      height: 2,
+      types:['poison']
+    }
+  ];
+
+  function getAll() {
+    return pokemonList;
   }
-];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  
+  return {
+    getAll,
+    add
+  }
+})();
+
 
 // we iterate through the pokemonList array displaying each objects name and height in a HTML paragraph element with the class "pokemon-card".
 // if a pokemons height is larger than 1, the string "- Wow, that's big!" will be added at the end
@@ -31,4 +47,4 @@ function myLoopFunction(pokemon) {
   }
 }
 
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
